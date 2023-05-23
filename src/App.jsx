10d187990bbe0d7fa99a9e806a3f0a004f16a650
mbunix <bellianotieno24@gmail.com>
2023-05-23 15:44:48 +0300

    merge resolves

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,88 @@
-import { useState, useEffect, useContext, createContext } from "react";
-import { TodoForm } from "./TodoForm";
-import { TodoList } from "./TodoList";
-
+import { useState, useEffect, useContext, createContext } from 'react'
+import { TodoForm } from './TodoForm'
+import { TodoList } from './TodoList'
+import './App.css'
 // Create a context for the todos
-export const TodoContext = createContext();
+export const TodoContext = createContext()
 
 // Custom hook to access the todo context
-export const useTodoContext = () => useContext(TodoContext);
+export const useTodoContext = () => useContext(TodoContext)
 
 const TodoProvider = ({ children }) => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
   const [todoToUpdate, settodoToUpdate] = useState({})
 
   // Load todos from local storage on component mount
   useEffect(() => {
-    const storedTodos = localStorage.getItem("todos");
+    const storedTodos = localStorage.getItem('todos')
     if (storedTodos) {
-      setTodos(JSON.parse(storedTodos));
+      setTodos(JSON.parse(storedTodos))
     }
-  }, []);
+  }, [])
 
   // Save todos to local storage whenever they change
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addTodo = (title, description, completion_date) => {
-    const newTodo = { id: Date.now(), title, description, completion_date, isCompleted: false };
-    setTodos([...todos, newTodo]);
-  };
+    const newTodo = {
+      id: Date.now(),
+      title,
+      description,
+      completion_date,
+      isCompleted: false
+    }
+    setTodos([...todos, newTodo])
+  }
 
-  const deleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+  const deleteTodo = id => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos(updatedTodos)
     settodoToUpdate({})
-  };
+  }
 
-  const updateTodo =  (id, title, description, completion_date)=> {
+  const updateTodo = (id, title, description, completion_date) => {
     // console.log(title,description,id,completion_date)
-    const updatedTodos = todos.map((todo) =>
+    const updatedTodos = todos.map(todo =>
       todo.id === id ? { ...todo, title, description, completion_date } : todo
-    );
-    settodoToUpdate({id,title,description,completion_date})
+    )
+    settodoToUpdate({ id, title, description, completion_date })
 
-    setTodos(updatedTodos);
-  };
+    setTodos(updatedTodos)
+  }
 
-  const completeTodo = (id) =>{
-    const updatedTodos = todos.map(todo=> {
-      todo.id === id ? {...todo, isCompleted: true}: todo
+  const completeTodo = id => {
+    const updatedTodos = todos.map(todo => {
+      todo.id === id ? { ...todo, isCompleted: true } : todo
     })
     setTodos(updatedTodos)
   }
 
   return (
-    <TodoContext.Provider value={{ todos, addTodo, deleteTodo, updateTodo, completeTodo ,todoToUpdate}}>
+    <TodoContext.Provider
+      value={{
+        todos,
+        addTodo,
+        deleteTodo,
+        updateTodo,
+        completeTodo,
+        todoToUpdate
+      }}
+    >
       {children}
     </TodoContext.Provider>
-  );
-};
+  )
+}
 
 const App = () => {
   return (
     <TodoProvider>
-      <h1>Todo App</h1>
+      <h1 className='todo-title'>Todo App</h1>
       <TodoForm />
       <TodoList />
     </TodoProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,36 +1,39 @@
-import { useState } from "react";
-import { useTodoContext } from "./App";
-import "./TodoList.css";
+import { useState } from 'react'
+import { useTodoContext } from './App'
+// import './TodoList.css'
+import './App.css'
 
 export const TodoList = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [completionDate, seCompletionDate] = useState("");
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [completionDate, seCompletionDate] = useState('')
 
-  const { todos, deleteTodo, updateTodo, completeTodo } = useTodoContext();
+  const { todos, deleteTodo, updateTodo, completeTodo } = useTodoContext()
 
   return (
     <>
-      <div className="todos">
-        {todos.map((todo) => (
-          <div className="todo" key={todo.id}>
-            <input type="checkbox" />
-            <div>
+      <div className='todos'>
+        {todos.map(todo => (
+          <div className='todo' key={todo.id}>
+            <input type='checkbox' />
+            <div className='todo-dets'>
               <p>Title: {todo.title} </p>
               <p>Description: {todo.description}</p>
               <p>Completion Date: {todo.completion_date}</p>
             </div>
-            <div className="buttons">
-              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <div className='todo-btns'>
+              <button className='todo-btn' onClick={() => deleteTodo(todo.id)}>
+                Delete
+              </button>
               <button
-                onClick={() =>{
+                onClick={() => {
                   updateTodo(
                     todo.id,
                     todo.title,
                     todo.description,
                     todo.completion_date
-                  )}
-                }
+                  )
+                }}
               >
                 Update
               </button>
@@ -39,5 +42,5 @@ export const TodoList = () => {
         ))}
       </div>
     </>
-  );
-};
+  )
+}
