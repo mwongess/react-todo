f595fad4b59d898ef9e35be66329175069f1407f
mbunix <bellianotieno24@gmail.com>
2023-05-23 12:23:53 +0300

    css and logic update

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,32 +1,25 @@
 import { useTodoContext } from "./App";
+import "./TodoList.css"
 export const TodoList = () => {
   const { todos, deleteTodo, updateTodo ,completeTodo} = useTodoContext();
 
   return (
     <div className="todos">
       {todos.map((todo) => (
-        <div key={todo.id}>
+        <div className="todo" key={todo.id}>
           <input type="checkbox" />
           <div>
             <p>Title: {todo.title} </p>
             <p>Description: {todo.description}</p>
             <p>Completion Date: {todo.completion_date}</p>
           </div>
-          <div>
-            {/* <button onClick={()=>updateTodo(t)}>Update</button> */}
+          <div className="buttons">
             <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <button onClick={()=> updateTodo(todo.id, todo.title, todo.description, todo.completion_date)}>Update</button>
           </div>
         </div>
       ))}
-      {/* <div >
-        <p>Title</p>
-        <p>Desc</p>
-        <p>Date</p>
-      </div><div>
-        <p>Title</p>
-        <p>Desc</p>
-        <p>Date</p>
-      </div> */}
+    
     </div>
   );
 };
